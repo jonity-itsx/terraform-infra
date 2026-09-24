4efdeed9f49e5b613d20d341511f1cd2927486b6
Jon-ITSX <jon.eskilsson@chasacademy.se>
2026-09-24 10:57:23 +0200

    Kommentera bort OS Login-sinken tills CI har logging.sinks.create

--- team/outputs.tf
+++ team/outputs.tf
@@ -2,7 +2,9 @@ output "jumphost_external_ip" {
   description = "External IP of the jumphost instance"
   value       = google_compute_instance.jumphost.network_interface[0].access_config[0].nat_ip
 }
-output "oslogin_sink_writer_identity" {
-  description = "Sinks writer identity, needs roles/pubsub.publisher on topic"
-  value       = google_logging_project_sink.oslogin_audit.writer_identity
-}
+
+# Aktiveras när sinken finns i koden igen, se oslogin-audit.tf.
+# output "oslogin_sink_writer_identity" {
+#   description = "Sinks writer identity, needs roles/pubsub.publisher on topic"
+#   value       = google_logging_project_sink.oslogin_audit.writer_identity
+# }
